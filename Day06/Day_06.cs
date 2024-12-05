bf0cde9492ecadb6ce2615b3fd110ab987a97f68
Zvmcevap <60576794+Zvmcevap@users.noreply.github.com>
2024-12-05 09:41:23 +0100

    Readying for tomorrow

---
+++ Day06/Day_06.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2024.Core;
+
+
+namespace AdventOfCode2024
+{
+
+	internal class Day_06: Solver
+	{
+		private readonly List<string> _inputLines = FileReader.ReadInput("Day06/input");
+
+
+		public Day_06() { }
+
+		public override void PartOne()
+		{
+
+		}
+
+		public override void PartTwo()
+		{
+		}
+	}
+}
